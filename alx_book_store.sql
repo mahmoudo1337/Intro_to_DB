2dd95b8518d1ecd96862d4ba42e97a0f333e2683
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-07-26 02:54:04 +0300

    initiating db

---
+++ alx_book_store.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Books (
+    book_id PRIMARY KEY,
+    title VARCHAR(30),
+    FOREIGN KEY (author_id) REFERENCES (Authors),
+    price DOUBLE,
+    publication_date DATE
+)
+
+CREATE TABLE Authors (
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+)
+
+CREATE TABLE Customers (
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215)
+    email VARCHAR(215)
+    address TEXT
+)
+
+CREATE TABLE Orders (
+    order_id PRIMARY KEY,
+    Foreign Key (customer_id) REFERENCES (Customers),
+    order_date DATE
+)
+
+CREATE TABLE Order_Details(
+    orderdetail_id PRIMARY KEY,
+    Foreign Key (order_id) REFERENCES (Orders),
+    Foreign Key (book_id) REFERENCES (Books),
+    quantity DOUBLE
+)
