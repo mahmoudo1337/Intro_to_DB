47c5e0eb76b156aa2ce09beee2e8833d08642304
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-07-26 18:40:28 +0300

    creating DB

--- MySQLServer.py
+++ MySQLServer.py
@@ -6,7 +6,7 @@ mydb = mysql.connector.connect(host="localhost", user="root", password="root", d
 mycursor = mydb.cursor()
 try:
   mycursor.execute("""
-CREATE TABLE IF NOT EXISTS alx_book_store (
+CREATE DATABASE IF NOT EXISTS alx_book_store (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE
